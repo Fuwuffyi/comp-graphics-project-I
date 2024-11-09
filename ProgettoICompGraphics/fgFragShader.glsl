0a228ee3a12df7b9da29ab84d238ee24ece103e8
Fuwuffyi <luca.palazzini3@studio.unibo.it>
2024-11-09 18:08:12 +0100

    Improve CRT shader

--- ProgettoICompGraphics/fgFragShader.glsl
+++ ProgettoICompGraphics/fgFragShader.glsl
@@ -6,21 +6,24 @@ in vec2 uv;
 
 uniform float timer;
 
+// Slightly blurred borders
 void drawVignette(inout vec3 color, vec2 uv) {    
     float vignette = uv.x * uv.y * (1.0 - uv.x) * (1.0 - uv.y);
     vignette = clamp(pow(16.0 * vignette, 0.3), 0.0, 1.0);
     color *= vignette;
 }
 
+// Lines on screen for CRT effect
 void drawScanlines(inout vec3 color, vec2 uv) {
-    float scanline = clamp(0.95 + 0.05 * cos(3.14 * (uv.y + 0.008 * timer) * 240.0 * 1.0), 0.0, 1.0);
+    float scanline = clamp(0.95 + 0.05 * cos(3.14 * (uv.y + 0.008 * timer * 4.0) * 240.0 * 1.0), 0.0, 1.0);
     float grille = 0.85 + 0.15 * clamp(1.5 * cos(3.14 * uv.x * 640.0 * 1.0), 0.0, 1.0);    
     color *= scanline * grille * 1.2;
 }
 
 void main() {
-    vec4 col = vec4(vec3(1.0), 0.2);
-    drawVignette(col.xyz, uv);
-    drawScanlines(col.xyz, uv);
-	fragColor = col;
+    vec3 col = vec3(1.0);
+    drawVignette(col, uv);
+    drawScanlines(col, uv);
+    // Invert color so it's mostly black
+	fragColor = vec4(vec3(1.0) - col, 0.2);
 }
